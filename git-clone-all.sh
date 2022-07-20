7f6cebfb58915eb03360b5f26977e71cbcb7df40
Sarran Vidyakumar <svidyakumar@ph.lacounty.gov>
2022-07-19 23:14:39 -0700

    checkpoint

--- git-clone-all.sh
+++ git-clone-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ..
 
-repos=(health360-tenant-service.git)
+repos=(health360-tenant-service.git health360-administration-service.git)
 for repo in ${repos[*]}; do
     git clone https://github.com/jazzarran/${repo}
  done
